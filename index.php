e0cf578212b21fc710308df04aeba3a41b612c66
Jared Howland <git@jaredhowland.com>
2016-09-27 16:06:06 -0600

    [FIX] Malformed data

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'config.php';
 
-// $undergrad = new undergrad;
+$undergrad = new undergrad;
 // $undergrad->colleges();
 // $undergrad->departments();
 // $undergrad->programs();
 // $undergrad->courses();
-// $undergrad->faculty();
+$undergrad->faculty();
 
 // $grad = new grad;
 // $grad->colleges();
